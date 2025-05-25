578250c65a85e8637351106200e6f2a115695b89
unknown <waqar.christiano@gmail.com>
2025-05-24 23:18:12 -0500

    /cors update

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://my-job-site-x1or.vercel.app/',
     credentials: true
 }
 app.use(cors(corsOptions));
