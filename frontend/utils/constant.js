4f2d421b2b5edd7240df5bcbf6f7aa1958207873
unknown <waqar.christiano@gmail.com>
2025-05-24 22:54:28 -0500

    /routes updated

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT="http://localhost:5000/api/v1/user";
+export const USER_API_END_POINT="https://my-job-site.vercel.app/api/v1/user";
 // export const JOB_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/job";
 // export const Application_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/application";
 // export const Company_API_END_POINT="https://jobwebsite-youtube-1.onrender.com/api/v1/company";
-export const JOB_API_END_POINT="http://localhost:5000/api/v1/job";
-export const Application_API_END_POINT="http://localhost:5000/api/v1/application";
-export const Company_API_END_POINT="http://localhost:5000/api/v1/company";
+export const JOB_API_END_POINT="https://my-job-site.vercel.app/api/v1/job";
+export const Application_API_END_POINT="https://my-job-site.vercel.app/api/v1/application";
+export const Company_API_END_POINT="https://my-job-site.vercel.app/api/v1/company";
